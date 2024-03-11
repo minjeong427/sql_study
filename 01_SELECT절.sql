cbd0a2db3d3eb9685d01359f522b3c1b1d645858
minjeong <>
2024-03-11 09:44:30 +0900

    select절

---
+++ 01_SELECT절.sql
@@ -0,0 +1,77 @@
+
+-- 오라클의 한 줄 주석
+
+/*
+여러 줄 주석
+*/
+
+-- SELECT [컬럼명(여러 개 가능)] FROM [테이블 이름]
+SELECT * FROM employees;
+
+select 
+    employee_id, 
+    first_name, 
+    last_name
+from 
+    employees;
+    
+select email, phone_number, hire_date
+from employees;
+
+-- 컬럼을 조회하는 위치에서 * / + - 연산이 가능합니다.
+select
+    employee_id,
+    first_name,
+    last_name,
+    salary,
+    salary + salary*0.1 as 성과금포함
+from employees;
+
+-- NULL 값의 확인 (숫자 0이나 공백과는 다른 존재입니다.)
+select department_id, commission_pct
+from employees;
+
+-- alias (컬럼명, 테이블명의 이름을 변경해서 조회합니다.)
+select
+    first_name as 이름,
+    last_name as 성,
+    salary as 급여
+from employees;
+
+/*
+오라클은 홑따옴표('')로 문자를 표현하고, 문자열 안에 홑따옴표 특수기호를
+표현하고 싶다면 ''를 2번 연속으로 쓰면 됩니다.
+문자열 연결 기호는 || 입니다. 문자열의 덧셈 연산을 허용하지 않습니다.
+*/
+select
+    first_name || ' ' || last_name || '''s salary is $' || salary as 급여내역
+from employees;
+
+--  DISTINCT(중복 행의 제거)
+select department_id from employees;
+select distinct department_id from employees;
+
+--ROWNUM, ROWID
+-- (**로우넘: 쿼리에 의해 반환되는 행 번호를 반환)
+-- (로우아이디: 데이터베이스 내의 행의 주소를 반환)
+select rownum, rowid, employee_id
+from employees;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
